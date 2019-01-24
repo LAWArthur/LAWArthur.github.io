778b3bd27529085bb61a09c850ae0f21473178eb
LAWArthur <jincai3335_micro@163.com>
2019-01-24 18:17:18 +0800

    add access code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const accessCode = Math.floor(Math.random() * 1000000);
 ($(()=>{
+    // ^_^
     $("#penwar").click(()=>{
-        window.location.href = "penwar/penwar.html";
+        if(prompt("Access code") == accessCode)
+        window.location.href = "\u0070\u0065\u006e\u0077\u0061\u0072\u002f\u0070\u0065\u006e\u0077\u0061\u0072\u002e\u0068\u0074\u006d\u006c";// ^_^
     });
     $("#alotools").click(()=>{
         window.location.href = "alotools/alotools.html";
