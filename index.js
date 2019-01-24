a71fa30c3a036f60342198e7609605f90b99fb4a
LAWArthur <jincai3335_micro@163.com>
2019-01-24 17:12:16 +0800

    completed web build

---
+++ index.js
@@ -0,0 +1,8 @@
+($(()=>{
+    $("#penwar").click(()=>{
+        window.location.href = "penwar/penwar.html";
+    });
+    $("#alotools").click(()=>{
+        window.location.href = "alotools/alotools.zip";
+    });
+}))()
---
+++ penwar/penwar.js
@@ -0,0 +1,8 @@
+($(()=>{
+    $("#win").click(()=>{
+        window.location.href = "https://raw.githubusercontent.com/LAWArthur/PenWar/master/Releases/setup.exe";
+    });
+    $("#github").click(()=>{
+        window.location.href = "https://github.com/LAWArthur/PenWar/";
+    });
+}))()
