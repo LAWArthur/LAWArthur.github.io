1ab54d66b8b81665346a92c775565b8adf954cab
LAWArthur <jincai3335_micro@163.com>
2019-03-10 20:49:24 +0800

    nothing

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const accessCode = Math.floor(Math.random() * 1000000);
 ($(()=>{
     let sel = $(".option");
     
     // ^_^
     $("#penwar").click(()=>{
-        if(prompt("Access code") == accessCode)
         window.location.href = "\u0070\u0065\u006e\u0077\u0061\u0072\u002f\u0070\u0065\u006e\u0077\u0061\u0072\u002e\u0068\u0074\u006d\u006c";// ^_^
     });
     $("#alotools").click(()=>{
--- penwar/penwar.js
+++ penwar/penwar.js
@@ -1,11 +1,8 @@
-const accessCode = Math.floor(Math.random() * 100000000);
 ($(()=>{
     $("#win").click(()=>{
-        if(prompt("Access code") == accessCode)
         window.location.href = "https://raw.githubusercontent.com/LAWArthur/PenWar/master/Releases/setup.exe";
     });
     $("#github").click(()=>{
-        if(prompt("Access code") == accessCode)
         window.location.href = "https://github.com/LAWArthur/PenWar/";
     });
 }))()
